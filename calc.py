71b5747eb611ebc1c2d6bc3b789da15c8bad634f
roopesh-kumar-sharma <roopesharma1335@gmail.com>
2024-06-09 12:53:43 +0545

    This is my inital file

---
+++ calc.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("ENter the age: "))
+
+if age >= 18:
+    if name.startswith == "A":
+        print(f"You are {name} and you are eligible for voting.")
+    else:
+        print(f"You are not eligile as your name doesnt start with A")
+else:
+    print("You havenot reached the age")
